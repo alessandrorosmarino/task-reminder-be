785c089fff6b4125f78422a2121f67ac0a8e14e3
alessandrorosmarino <alessandrorosmarino@gmail.com>
2023-06-01 15:40:38 +0100

    testing

--- src/main/java/com/alessandro/rosmarino/task_reminder/web/configuration/TaskCorsConfiguration.java
+++ src/main/java/com/alessandro/rosmarino/task_reminder/web/configuration/TaskCorsConfiguration.java
@@ -5,14 +5,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 @Configuration
 public class TaskCorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String originAllowed = "localhost";
+        try (final DatagramSocket datagramSocket = new DatagramSocket()) {
+            datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
+            originAllowed = datagramSocket.getLocalAddress().getHostAddress();
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ORIGIN: " + originAllowed);
         registry.addMapping("/**")
                 .allowedMethods(CorsConfiguration.ALL)
                 .allowedHeaders(CorsConfiguration.ALL)
-                .allowedOrigins(CorsConfiguration.ALL);
+                .allowedOriginPatterns("http://"+originAllowed);
     }
 }
